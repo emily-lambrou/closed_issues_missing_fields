21838bf2f0febd583d9bf7f7f78579fdabf396bc
Emily <emily-l93@hotmail.com>
2024-11-07 13:24:50 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,21 +2,41 @@ import graphql
 import config
 from logger import logger
 
+# List of essential fields we want to check for
+ESSENTIAL_FIELDS = [
+    "Status", "Due Date", "Time Spent", "Release",
+    "Estimate", "Priority", "Size", "Week"
+]
+
 def prepare_missing_fields_comment(issue: dict, assignees: dict):
     """
-    Prepare the comment from the given arguments and return it
+    Prepare a comment for an issue with missing required fields and return it.
+    Only consider essential fields, and ignore fields marked as old, such as
+    "Time Spent OLD" and "Estimate OLD".
     """
-
+    # Check for missing essential fields
+    missing_fields = [
+        field for field in ESSENTIAL_FIELDS if field not in issue
+    ]
+    
+    # If no essential fields are missing, do not generate a comment
+    if not missing_fields:
+        logger.info(f'Issue #{issue["number"]} has no missing essential fields.')
+        return None
+    
+    # Prepare the comment string
     comment = ''
     if assignees:
         for assignee in assignees:
             comment += f'@{assignee["login"]} '
     else:
         logger.info(f'No assignees found for issue #{issue["number"]}')
-
-    comment += f'Kindly set the missing required fields for the project: Status, Due Date, Time Spent, Release, Estimate, Priority, Size, Week.'
+    
+    # Append message about missing fields
+    missing_fields_str = ', '.join(missing_fields)
+    comment += f'Kindly set the missing required fields for the project: {missing_fields_str}.'
     logger.info(f'Issue {issue["title"]} | {comment}')
-
+    
     return comment
 
 def check_comment_exists(issue_id, comment_text):
@@ -26,8 +46,3 @@ def check_comment_exists(issue_id, comment_text):
         if comment_text in comment.get('body', ''):
             return True
     return False
-
-
-
-
-
