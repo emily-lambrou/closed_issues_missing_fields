b9ec51015902ded682baa6a709135dd661f85512
Emily <emily-l93@hotmail.com>
2024-11-08 12:30:41 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -92,51 +92,6 @@ def notify_missing_duedate():
                     graphql.add_issue_comment(issue_id, comment)    
                 logger.info(f'Comment added to issue {issue_id}')
 
-def notify_missing_timespentold():
-    if config.is_enterprise:
-        issues = graphql.get_project_issues_timespentold(
-            owner=config.repository_owner,
-            owner_type=config.repository_owner_type,
-            project_number=config.project_number,
-            timespentold_field_name=config.timespentold_field_name,
-            filters={'empty_timespentold': True, 'closed_only': True}
-        )
-    else:
-        # Get the issues
-        issues = graphql.get_repo_closed_issues(
-            owner=config.repository_owner,
-            repository=config.repository_name
-        )
-
-    # Check if there are issues available
-    if not issues:
-        logger.info('No issues has been found')
-        return
-
-    for projectItem in issues:
-        issue = projectItem['content']
-
-        # Get the list of assignees
-        assignees = issue['assignees']['nodes']
-
-        comment_text = f"Kindly set the missing required fields for the project: Status, Due Date, Time Spent, Release, Estimate, Priority, Size, Week."
-        issue_id = issue['id']
-        
-        # Check if the comment already exists
-        if not utils.check_comment_exists(issue_id, comment_text):
-            if config.notification_type == 'comment':
-                # Prepare the notification content
-                comment = utils.prepare_missing_fields_comment(
-                    issue=issue,
-                    assignees=assignees, 
-                )
-    
-                if not config.dry_run:
-                    # Add the comment to the issue
-                    graphql.add_issue_comment(issue_id, comment)    
-                logger.info(f'Comment added to issue {issue_id}')
-
-
 def notify_missing_timespent():
     if config.is_enterprise:
         issues = graphql.get_project_issues_timespent(
@@ -224,53 +179,7 @@ def notify_missing_release():
                     # Add the comment to the issue
                     graphql.add_issue_comment(issue_id, comment)    
                 logger.info(f'Comment added to issue {issue_id}')
-
-
-def notify_missing_estimateold():
-    if config.is_enterprise:
-        issues = graphql.get_project_issues_estimateold(
-            owner=config.repository_owner,
-            owner_type=config.repository_owner_type,
-            project_number=config.project_number,
-            estimateold_field_name=config.estimateold_field_name,
-            filters={'empty_estimateold': True, 'closed_only': True}
-        )
-    else:
-        # Get the issues
-        issues = graphql.get_repo_closed_issues(
-            owner=config.repository_owner,
-            repository=config.repository_name
-        )
-
-    # Check if there are issues available
-    if not issues:
-        logger.info('No issues has been found')
-        return
-
-    for projectItem in issues:
-        issue = projectItem['content']
-
-        # Get the list of assignees
-        assignees = issue['assignees']['nodes']
-
-        comment_text = f"Kindly set the missing required fields for the project: Status, Due Date, Time Spent, Release, Estimate, Priority, Size, Week."
-        issue_id = issue['id']
-        
-        # Check if the comment already exists
-        if not utils.check_comment_exists(issue_id, comment_text):
-            if config.notification_type == 'comment':
-                # Prepare the notification content
-                comment = utils.prepare_missing_fields_comment(
-                    issue=issue,
-                    assignees=assignees, 
-                )
-    
-                if not config.dry_run:
-                    # Add the comment to the issue
-                    graphql.add_issue_comment(issue_id, comment)    
-                logger.info(f'Comment added to issue {issue_id}')
-
-
+      
 def notify_missing_estimate():
     if config.is_enterprise:
         issues = graphql.get_project_issues_estimate(
@@ -454,14 +363,13 @@ def main():
 
     notify_missing_status()
     notify_missing_duedate()
-    notify_missing_timespentold()
     notify_missing_timespent()
     notify_missing_release()
-    notify_missing_estimateold()
     notify_missing_estimate()
     notify_missing_priority()
     notify_missing_size()
     notify_missing_week()
+  
 
 if __name__ == "__main__":
     main()
